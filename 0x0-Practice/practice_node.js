7c5f22c8023ece90abacb7714dcf99bd15f7971e
Hinata Hyuga <hinata@Hinatas-MBP.home>
2020-05-09 17:33:09 -0400

    test

---
+++ 0x0-Practice/practice_node.js
@@ -0,0 +1,8 @@
+// 1
+// console.log("hello world")
+// 2
+// If you want to use single quotes and include a quote inside a string, use an escape character \ before the quote.
+// console.log('Matt\'s awesome.');
+// 3
+// We can do arithmetic with numbers
+// console.log(100 + 100);
