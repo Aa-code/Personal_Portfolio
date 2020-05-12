2edfffb47123f514fbc897d89453930c56a6056f
Hinata Hyuga <hinata@Hinatas-MBP.home>
2020-05-11 22:13:32 -0400

    [added notes]

--- 0x0-Practice/loops_while_for.js
+++ 0x0-Practice/loops_while_for.js
@@ -1,3 +1,10 @@
 // How to talk to my terminal using node.js,  with this problem below i am asking is 1 greater than 2, and when i ask node its anser will be false. Thats a boolean expression.
-// console.log(1 > 2);
-// 
+// console.log(1 > 2); = false
+// > greater than
+{/* < less than
+== loosely equals to
+=== strictly equals
+>= greater than or equal to
+<= less than or equal to
+!= is not equal
+//  */}
--- 0x0-Practice/practice_node.js
+++ 0x0-Practice/practice_node.js
@@ -63,7 +63,7 @@
 
 // b. Repeat using template literals
 // Using template literals to solve a math probelm 5 + 10
-const num1 = 5
-const num2 = 10
+// const num1 = 5
+// const num2 = 10
 // console.log(`The sum of ${num1} and ${num2} is 15`);
-console.log((num1+num2))
+// console.log((num1+num2))
