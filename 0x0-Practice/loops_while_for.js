83a4dacf2fc3edf2d76f3d54059664b66928c947
Hinata Hyuga <hinata@Hinatas-MBP.home>
2020-05-11 22:10:37 -0400

    [Asking the terminal true or false questions using boolean expressions]

---
+++ 0x0-Practice/loops_while_for.js
@@ -0,0 +1,3 @@
+// How to talk to my terminal using node.js,  with this problem below i am asking is 1 greater than 2, and when i ask node its anser will be false. Thats a boolean expression.
+// console.log(1 > 2);
+// 
